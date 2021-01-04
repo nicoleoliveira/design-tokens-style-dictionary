c5cfa37d0052e3e975e56ef3308678db7533aa22
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-01-04 10:57:01 -0300

    first commit

---
+++ build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.swift
+//
+// Do not edit directly
+// Generated on Wed, 30 Dec 2020 16:35:20 GMT
+//
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let fontBase = CGFloat(16.00)
+    public static let fontLarge = CGFloat(32.00)
+    public static let fontMedium = CGFloat(16.00)
+    public static let fontSmall = CGFloat(12.00)
+}
